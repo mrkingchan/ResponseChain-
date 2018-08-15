e3fdf4c1fe0df754d771cf7daaec85c799c026fd
mrkingchan <1203710130@qq.com>
2018-08-15 13:46:26 +0800

    Initial Commit

---
+++ ResponseChain/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ResponseChain
+//
+//  Created by Macx on 2018/8/15.
+//  Copyright © 2018年 Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ResponseChain/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ResponseChain
+//
+//  Created by Macx on 2018/8/15.
+//  Copyright © 2018年 Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
